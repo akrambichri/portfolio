43e8734fe9396a84038e359324fc219a149072d9
akrambichri <47328724+akrambichri@users.noreply.github.com>
2021-04-23 09:48:09 +0200

    fixing build

--- pages/components/Work/modal.js
+++ pages/components/Work/modal.js
@@ -1,6 +1,4 @@
 import React, { useState, useRef, useEffect } from "react";
-import Slider from "./Carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { ReactVideo } from "reactjs-media";
 
 export default function ProjectModal(props) {
